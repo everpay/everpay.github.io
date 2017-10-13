f59f50e8cdc55aee5b16fcb4163694896560ace1
Elektropay <richie@elektropay.com>
2017-10-13 09:15:16 -0400

    Create index.php

---
+++ _pages/company/contact/index.php
@@ -0,0 +1,19 @@
+<?php
+date_default_timezone_set ("PST8PDT");
+print "<b>Date/Time (PST) Built: ".date("Ymd H:i:s e")." </b><p>\n";
+print "Local time from the browser: <br>"."<script src='date.js'></script>"
+?>
+
+<form name="contact" action="contact.html" netlify>
+  <p>
+    <label>Your Name:</label>
+    <input type="text" name="name">
+  </p>
+  <p>
+    <label>Your Email</label>
+    <input type="email" name="email">
+  </p>
+  <p>
+    <button>Send</button>
+  </p>
+</form>
